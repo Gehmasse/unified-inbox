28610926e8a7046c0185b28515c589e5194a08ab
Leo Nickl <leo@leonickl.de>
2025-03-27 23:14:27 +0100

    description

--- env.example.php
+++ env.example.php
@@ -2,9 +2,10 @@
 
 return [
     // an array of all email accounts
+    // the key (here 'default-label') is how you want to name the account
     // all parameters from new Account() constructor are allowed here
     'accounts' => [
-        'default' => [
+        'default-label' => [
             'title' => 'default',
             'host' => '***',
             'username' => '***',
